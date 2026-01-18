47852bfc3454c2321928d959b1ccabc9e7286735
Kayetan Protas <kayetanp@gmail.com>
2026-01-18 16:12:41 -0500

    css done

---
+++ logic.js
@@ -0,0 +1,5 @@
+const rows = 8;
+cosnt cols = 8;
+const board = document.getElementById("Game-board");
+const gameStatus = document.getElementById("game-status");
+const restartButton = document.getElementById("restart-button");
